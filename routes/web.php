2aa7d0cd72257c5916a5e57c840f3c2ef299a2c4
YoSoyBiss <fugitivo1416@gmail.com>
2024-08-19 15:24:01 -0600

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	renamed:    resources/views/registro.php -> resources/views/registro.blade.php
    	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/registro', [AuthController::class, 'showRegistrationForm'])->name('registro');
+Route::post('/registro', [AuthController::class, 'register']);
+
+Route::get('/Recuperar_contraseña', [AuthController::class, 'showForgotPasswordForm'])->name('Recuperar_contraseña');
+Route::post('/Recuperar_contraseña', [AuthController::class, 'sendPasswordResetLink']);
 
 Route::get('/', function () {
     return view('welcome');
